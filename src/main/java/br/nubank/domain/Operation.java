890db13e7c4311a7b33f3debd5625358c36b168d
GuilhermePortella <guilhermeportella2@gmail.com>
2025-08-31 15:00:23 -0300

    chore: Untrack ignored files

--- src/main/java/br/nubank/domain/Operation.java
+++ src/main/java/br/nubank/domain/Operation.java
@@ -4,17 +4,6 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 public record Operation(OperationType type, BigDecimal unitCost, long quantity) {
-    
-//    public Operation{
-//        
-//        Objects.requireNonNull(type, "tipo");
-//        Objects.requireNonNull(unitCost, "custoUnitario");
-//        
-//        //remover essa verificação pois o doc diz que nao tera entradas erradas
-//        if (quantity <= 0) throw new IllegalArgumentException("quantity must be > 0");
-//        
-//        
-//    }
-   
+      
 }
  
